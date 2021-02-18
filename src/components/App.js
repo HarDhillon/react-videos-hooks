5b0e80f755fbede0be175f85ebb4c3f379e23fa5
Har Dhillon <har7173@gmail.com>
2021-02-18 19:23:37 +0400

    search bar is controlled and updates value as user types

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component{
   render() {
-    
+
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
