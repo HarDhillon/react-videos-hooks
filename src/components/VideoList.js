3a98974ce845c5fecf8b96c05a34f7061a502328
Har Dhillon <har7173@gmail.com>
2021-03-03 15:19:56 +0400

    adding iframe title and removing some console logs

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,6 @@ import VideoItem from './VideoItem';
 // rather than write props you can reference the items inside the props immediately
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    console.log(video)
     return <VideoItem onVideoSelect={onVideoSelect} key={video.id.videoId} video={video}/>
   });
   // therefore we dont need to write props.videos
