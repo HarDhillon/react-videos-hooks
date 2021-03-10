a01a47398bba335d7b30584f38d305910ffbde34
Har Dhillon <har7173@gmail.com>
2021-03-10 13:12:54 +0400

    creating a custom hook from code inside app.js

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import youtube from "../apis/youtube"
+
+// creating custom hooks: 
+//  If you give me "..." I will give you "..."
+// if you give me a "default search" term I will give you "a list of videos, and a way to update the list"
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() =>{
+    search(defaultSearchTerm)
+  }, []);
+
+  const search = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    setVideos(response.data.items)
+    }
+
+    return [videos, search];
+};
+
+export default useVideos;
